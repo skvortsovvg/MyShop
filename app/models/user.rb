1ed8d1c8bd07b9209e31c68db31537498d20b9da
Skvortsovvg <vovanches@i.ua>
2022-12-03 15:27:47 +0200

    tests for oauth controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :omniauthable, omniauth_providers: [:github]
+         :omniauthable, omniauth_providers: [:github ]
 
   has_many :questions, class_name: "Question", foreign_key: "author_id"
   has_many :answers, class_name: "Answer", foreign_key: "author_id"
   has_many :votes
   has_many :voted_answers, through: :votes, source: :answer
+  
+  def self.find_for_oauth(auth)
+
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,4 +3,16 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   it { should validate_presence_of :email }
   it { should validate_presence_of :password }
+
+  describe '.find_for_oauth' do
+    let!(:user) { FactoryBot.create(:user) }
+    let!(:auth) { OmniAuth::AuthHash.new(provider: 'vk', uid: '123456') }
+
+    context 'user has authorization' do
+      it 'return user' do
+        user.authorizations.create(provider: 'vk', uid: '123456')
+        expect(User.find_for_oauth(auth)).to eq user
+      end
+    end
+  end
 end
