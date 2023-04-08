56a81b3e3794c108c5b1a2c74e07c6a70118ff2d
Skvortsovvg <vovanches@i.ua>
2023-04-09 00:30:52 +0300

    action cable corrections

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   # config.action_cable.url = "wss://example.com/cable"
-  # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
+  config.action_cable.allowed_request_origins = [ "http://31.172.65.115", /http:\/\/31.172.65.115*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
