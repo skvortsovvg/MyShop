23b61df3718a0ad9b1c3791ec904f9069bc665ec
Skvortsovvg <vovanches@i.ua>
2023-04-19 09:57:42 +0300

    actioncable test

--- app/controllers/api/v1/common_controller.rb
+++ app/controllers/api/v1/common_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::CommonController < ApplicationController
   skip_before_action :verify_authenticity_token
   def report
-    Payment.create(payload: request.body.string)
+    Payment.create(payload: request1.body.string)
     render json: Payment.last.payload
   end
   def reports
